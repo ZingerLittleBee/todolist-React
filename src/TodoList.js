c6596a3bf30e3d60efe612578b8a396fa012e18b
beeij <xyijmn@outlook.com>
2021-01-05 13:27:44 +0800

    todolist-React

---
+++ src/TodoList.js
@@ -0,0 +1,72 @@
+/**
+ * @author  Ye
+ * @date  2021/1/5 11:08 上午
+ * @describe
+ */
+import React, { Component, Fragment } from 'react'
+import './style.css'
+
+class TodoList extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputValue: '',
+      list: []
+    }
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div>
+          <label htmlFor='insertArea'>输入内容</label>
+          <input
+            id='insertArea'
+            className='input'
+            value={this.state.inputValue}
+            onChange={this.handlerInputChange.bind(this)}
+          />
+          <button onClick={this.handlerButtonClick.bind(this)}>提交</button>
+        </div>
+          <ul>
+            {
+              this.state.list.map((item, index) => {
+                return (
+                  <li
+                    key={index}
+                    onClick={this.handlerItemDelete.bind(this, index)}
+                    dangerouslySetInnerHTML={{__html: item}}
+                  >
+                      {/*{item}*/}
+                  </li>)
+              })
+            }
+          </ul>
+      </Fragment>
+    )
+  }
+
+  handlerInputChange(e) {
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+  handlerButtonClick() {
+    if (!this.state.inputValue) { return }
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: ''
+    })
+  }
+
+  handlerItemDelete(index) {
+    const list = [...this.state.list]
+    list.splice(index, 1)
+    this.setState({
+      list
+    })
+  }
+}
+
+export default TodoList
